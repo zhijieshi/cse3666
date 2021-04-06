4892b84394c90908e09f3e1c70da5a59b9642169
Jerry Shi <zshi@uconn.edu>
2021-04-05 21:59:55 -0400

    testing 64-bit operations.

---
+++ rv-examples/10-add.s
@@ -0,0 +1,30 @@
+# Test 64-bit operations in RARS
+# For UConn CSE 3666, by Jerry Shi
+      
+        # .text starts code segments
+        .text
+        .globl  main    # declare main to be global. Note it is ".globl"
+
+main:   
+	# try to find out the bits in destination registers and 
+	# compare them with the results in RARS 
+	
+	li	x1, 0x5A5
+
+	slli	x2, x1, 12
+	
+	add	x3, x1, x2
+	
+	slli	x4, x3, 24
+	
+	add	x5, x3, x4
+
+        slli	x6, x5, 44
+        
+        add	x7, x5, x6
+        
+        add	x8, x7, x7
+        
+        # system call to exit with code 0
+exit:   li      a7, 10                  # li is a pseudoinstruction
+        ecall                           # system call
