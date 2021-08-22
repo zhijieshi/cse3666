7387e113cfb38f48c0166b8898218d5298e5826b
Jerry Shi <zhijieshi@gmail.com>
2021-08-22 10:57:56 -0400

    add hello example

---
+++ rv-examples/01-hello.s
@@ -0,0 +1,22 @@
+#       RISC-V Example in RARS
+#       Comments.  Anything after # is comments.
+
+        # .data starts data segments
+        .data
+        # msg is a label to a string, an ASCII string ends with a NUL character
+        # we can also use ".string", instead of ".asciz"
+msg:    .asciz  "Hello, welcome to CSE 3666.\n"
+
+        # .text starts code segments
+        .text
+        .globl  main    # declare main to be global. Note it is ".globl"
+
+        # define a label, in code segment
+main:   
+        la      a0, msg         # load the address of the string
+        li      a7, 4           # set the system call number. 4 for printing a string
+        ecall                   # system call
+
+        # system call 10: exit with code 0
+exit:   li      a7, 10          # set the system call number
+        ecall                   # system call
