e71a3ccd5234a13d7864cc73fb4cbf3449625985
Jerry Shi <zshi@uconn.edu>
2021-09-17 10:38:02 -0400

    change to 32 bits

--- rv-examples/73-fact.s
+++ rv-examples/73-fact.s
@@ -7,19 +7,15 @@ main:
         li      a0, 19
         jal     ra, fact
         
-        # set a breakpoint to see all bits in a0
-        # 1 for decimal, 34 for hex
-        li      a7, 34
-        ecall
-
         # system call to exit with code 0
         li      a7, 10         # li is a pseudoinstruction
         ecall                  # system call
 
+# int fact(int n)
 fact:
-        addi    sp, sp, -16    # adjust stack for 2 DWs
-        sd      ra, 8(sp)      # save return address
-        sd      a0, 0(sp)      # save argument
+        addi    sp, sp, -8     # adjust stack for 2 words
+        sw      ra, 8(sp)      # save return address
+        sw      a0, 0(sp)      # save argument
 
 	# if n >= 1 goto ELSE
         addi    t0, zero, 1
@@ -32,12 +28,11 @@ fact:
 	# n >= 1
 ELSE: 
         addi    a0, a0, -1     # else decrement n  
-        jal     fact           # recursive call
-        ld      t0, 0(sp)      # restore original n
+        jal     ra, fact       # recursive call
+        lw      t0, 0(sp)      # restore original n
         mul     a0, a0, t0     # n * (n-1)!
 EXIT:   
-        ld      ra, 8(sp)      # restore return address
-        addi    sp, sp, 16     # pop 2 DWs from stack
+        lw      ra, 4(sp)      # restore return address
+        addi    sp, sp, 8      # pop 2 DWs from stack
         jalr    x0, 0(ra)      # and return
-
 
