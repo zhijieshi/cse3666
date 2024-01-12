c72a0615381ec57003bda8f28348ee3e30a7e21a
Jerry Shi <zshi@uconn.edu>
2024-01-12 09:53:30 -0500

    add.c to demo C to assembly code

---
+++ code-examples/add.c
@@ -0,0 +1,17 @@
+#include    <stdio.h>
+
+
+/* Use the following command line to generate assembly code 
+ 
+    gcc -S -c -masm=intel add.c
+*/
+
+int main()
+{
+    int a, b;
+
+    a =  1;
+    b =  20;
+    return a + b;
+}
+
