025aa7e6fa8f9f734f6731c9a025d820e37b7571
Jerry Shi <jerryshict@outlook.com>
2024-02-19 08:44:11 -0500

    update fp example

--- rv-examples/91-f2c-v2.s
+++ rv-examples/91-f2c-v2.s
@@ -23,13 +23,13 @@ exit:   addi    a7, x0, 10
 
 # float f2c(float f)
 f2c: 	
-	li	t0, 5
-	li	t1, 9
+	addi	 t0, x0, 5
+	addi	 t1, x0, 9
 	fcvt.s.w ft0, t0
 	fcvt.s.w ft1, t1
-	fdiv.s  ft0, ft0, ft1	# compute 5/9
-	li	t1, 32
+	fdiv.s   ft0, ft0, ft1	# compute 5/9
+	addi	 t1, x0, 32
 	fcvt.s.w ft1, t1
-	fsub.s  fa0, fa0, ft1	# compute fahr - 32
-	fmul.s  fa0, ft0, fa0	# multiply with 5/9
-        jalr    x0, ra, 0
+	fsub.s   fa0, fa0, ft1	# compute fahr - 32
+	fmul.s   fa0, ft0, fa0	# multiply with 5/9
+        jalr     x0, ra, 0
